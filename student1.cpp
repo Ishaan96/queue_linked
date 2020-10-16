b6c0c3aa2d01ea4db73446aa7fb7e7501760d432
Ishaan96 <ishaan.sengupta2@gmail.com>
2020-10-16 13:39:28 +0530

    Add files via upload
    Operator >,<

---
+++ student1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+class student
+{
+private:
+    std::string name;
+    int roll,marks;
+public:
+    student() {
+        std::cout << "Name:";
+        std::cin >> name;
+        std::cout << "Roll:";
+        std::cin >> roll;
+        std::cout << "Marks:";
+        std::cin >> marks;
+    }
+    bool operator>(student& s) const;
+    bool operator<(student& s) const;
+};
+bool student::operator>(student& s) const {
+    if (this->marks > s.marks)
+        return true;
+    else
+        return false;
+}
+bool student::operator<(student& s) const {
+    if (this->marks < s.marks)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    student s1;
+    student s2;
+    std::cout << std::boolalpha << (s2>s1);
+    return 0;
+}
