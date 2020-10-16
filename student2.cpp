d618e60c2bb7e67d03d000cd8e85ca91cd0a3ed2
Ishaan96 <ishaan.sengupta2@gmail.com>
2020-10-16 19:34:15 +0530

    Add files via upload
    Operator &&, ||

---
+++ student2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<string>
+class student
+{
+public:
+    std::string name;
+    int roll;
+    student(std::string n,int r) : name(n),roll(r){}
+};
+class subject1: public student
+{
+public:
+    int marks;
+    subject1(const student& s) :student(s)
+    {
+        std::cout << "Subject1 Marks:";
+        std::cin >> marks;
+    }
+};
+class subject2: public student
+{
+public:
+    int marks;
+    subject2(const student& s) : student(s)
+    {
+        std::cout << "Subject2 Marks:";
+        std::cin >> marks;
+    }
+};
+bool operator&&(const subject1& s1,const subject2& s2)
+{
+    return s1.marks>60 && s2.marks>60;
+}
+bool operator||(const subject1& s1,const subject2& s2)
+{
+    return s1.marks>90 || s2.marks>90;
+}
+class Application
+{
+public:
+    void check(const subject1& s1,const subject2& s2) const
+    {
+         if(s1 && s2) {
+             if (s1 || s2)
+                 std::cout << s1.name << " is granted direct promotion";
+             else
+                 std::cout << s1.name << " is allowed to apply";
+         }
+             else
+             std::cout << "Rejected";
+    }
+    friend bool operator&&(const subject1& s1,const subject2& s2);
+    friend bool operator||(const subject1& s1,const subject2& s2);
+};
+int main()
+{
+    student s("Ishaan",205);
+    subject1 s1(s);
+    subject2 s2(s);
+    Application* a = new Application;
+    a->check(s1,s2);
+    delete a;
+    return 0;
+}
